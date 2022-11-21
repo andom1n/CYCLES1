270e0eea3a376aaff13b77eb6223f78641a75f06
Andrei Tarasov <adnominator@bk.ru>
2022-11-21 21:29:41 +0500

    cycles

---
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -0,0 +1,26 @@
+package ru.netology.sqr;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class SQRServiceTest {
+    @Test
+    public void testReal () {
+        SQRService service = new SQRService();
+
+        int actual = service.border(200,300);
+        int expected = 3;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testUnreal () {
+        SQRService service = new SQRService();
+
+        int actual = service.border(300, 200);
+        int expected = 0;
+
+        Assertions.assertEquals(expected, actual);
+    }
+}
